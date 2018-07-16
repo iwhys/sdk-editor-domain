48812a5be6c75b345bc6d54e229e978e552aab81
devil <iwhs@qq.com>
2018-07-16 13:17:12 +0800

    重新定义方法编辑器相关接口

--- plugin-domain/src/main/java/com/iwhys/classeditor/domain/interf/MethodProxy.java
+++ plugin-domain/src/main/java/com/iwhys/classeditor/domain/interf/MethodProxy.java
@@ -2,16 +2,18 @@ package com.iwhys.classeditor.domain.interf;
 
 /**
  * Created on 16/07/2018 12:41
- * Description: 被编辑的方法信息实体
+ * Description: 被编辑的方法信息代理
  *
  * @author 王洪胜
  */
-public interface MethodEditor {
+public interface MethodProxy {
 
     Object getThis();
 
     Object[] gerArgs();
 
-    Object proceed(Object[] args);
+    Class<?> getReturnType();
+
+    Object invoke(Object[] args);
 
 }
